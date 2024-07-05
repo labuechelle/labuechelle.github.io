4407df5725cea668b20a41f669f1e9e675fd8806
Nathalie Buechel <labuechelle@gmail.com>
2024-07-05 14:23:23 +0200

    feature

--- jsindex.js
+++ jsindex.js
@@ -1,7 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
-document.getElementsByTagName("p")[0].style.fontSize = "2vw";
+console.log("Hello ")
+
