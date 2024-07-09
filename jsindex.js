ca81b574700d0e6562a980f5bcc38236e8052b0e
Nathalie Buechel <labuechelle@gmail.com>
2024-07-09 13:57:01 +0200

    colour

--- jsindex.js
+++ jsindex.js
@@ -1,2 +1,22 @@
-console.log("Hello ")
+// Find the banner
+let banner = document.getElementById("banner");
 
+// Change the text
+banner.innerHTML = "Wow, it's a banner!";
+
+// Find the button
+let button = document.getElementById("click1");
+
+// Make it do something when clicked
+button.onclick = function() {
+  alert("You clicked the button!");
+};
+
+let banner = document.getElementById("banner");
+let button = document.getElementById("colorButton");
+
+button.onclick = function() {
+  let colors = ["red", "blue", "green", "yellow", "purple"];
+  let randomColor = colors[Math.floor(Math.random() * colors.length)];
+  banner.style.backgroundColor = randomColor;
+};
